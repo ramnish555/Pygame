cb0e3cc35d6508f41ebbb1970413646a376daf45
ramnish555 <34938772+ramnish555@users.noreply.github.com>
2019-10-11 15:32:11 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        """Initialize game Settings."""
+        #Screen Settings
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
